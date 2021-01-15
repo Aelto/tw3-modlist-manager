7e7fb2fb50339db48f5a2ddcbc5757d87b28c064
Aelto <Aelto@users.noreply.github.com>
2021-01-15 19:51:46 +0100

    do not start a new instance if port is already in use

--- src/main.rs
+++ src/main.rs
@@ -18,39 +18,54 @@ mod utils;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-  let port: i32 = std::env::args()
+  let port: u16 = std::env::args()
     .nth(1)
-    .and_then(|n| n.parse::<i32>().ok())
+    .and_then(|n| n.parse::<u16>().ok())
     .unwrap_or(5000);
 
-  println!("starting server on port {}", port);
+  println!("checking if port is in use");
+  if let Err(_) = std::net::TcpStream::connect(("0.0.0.0", port)) {
+    println!("port is already in use");
 
-  HttpServer::new(|| {
-    App::new()
-    // home page
-    .service(web::resource("/").route(web::get().to(pages::root::render)))
-    .service(web::resource("/modlist/{modlist_name}").route(web::get().to(pages::modlist::render)))
+    // port is already used.
+    std::process::Command::new("cmd")
+      .arg("/C")
+      .arg("start")
+      .arg(format!("http://localhost:{}", port))
+      .output()?;
 
-    // static files
-    .service(fs::Files::new("/static", "./static"))
+    Ok(())
+  }
+  else {
+    println!("starting server on port {}", port);
 
-    // api endpoints
-    .service(
-      web::scope("/api")
-        .route("/modlist/initialize", web::post().to(api::modlist::initialize))
-        .route("/modlist/create", web::post().to(api::modlist::create_modlist))
-        .route("/modlist/install", web::post().to(api::modlist::install_modlist))
-        .route("/modlist/import", web::post().to(api::modlist::import_modlist))
-        .route("/modlist/remove-import", web::post().to(api::modlist::remove_imported_modlist))
-        .route("/modlist/load-imports", web::post().to(api::modlist::load_imports_modlist))
-        .route("/modlist/unload-imports", web::post().to(api::modlist::unload_imports_modlist))
-        .route("/modlist/move-import-up", web::post().to(api::modlist::move_imported_modlist_up))
-        .route("/modlist/move-import-down", web::post().to(api::modlist::move_imported_modlist_down))
-        .route("/modlist/view", web::post().to(api::modlist::view_modlist))
-    )
+    HttpServer::new(|| {
+      App::new()
+      // home page
+      .service(web::resource("/").route(web::get().to(pages::root::render)))
+      .service(web::resource("/modlist/{modlist_name}").route(web::get().to(pages::modlist::render)))
 
-  })
-  .bind(format!("127.0.0.1:{}", port))?
-  .run()
-  .await
+      // static files
+      .service(fs::Files::new("/static", "./static"))
+
+      // api endpoints
+      .service(
+        web::scope("/api")
+          .route("/modlist/initialize", web::post().to(api::modlist::initialize))
+          .route("/modlist/create", web::post().to(api::modlist::create_modlist))
+          .route("/modlist/install", web::post().to(api::modlist::install_modlist))
+          .route("/modlist/import", web::post().to(api::modlist::import_modlist))
+          .route("/modlist/remove-import", web::post().to(api::modlist::remove_imported_modlist))
+          .route("/modlist/load-imports", web::post().to(api::modlist::load_imports_modlist))
+          .route("/modlist/unload-imports", web::post().to(api::modlist::unload_imports_modlist))
+          .route("/modlist/move-import-up", web::post().to(api::modlist::move_imported_modlist_up))
+          .route("/modlist/move-import-down", web::post().to(api::modlist::move_imported_modlist_down))
+          .route("/modlist/view", web::post().to(api::modlist::view_modlist))
+      )
+
+    })
+    .bind(format!("127.0.0.1:{}", port))?
+    .run()
+    .await
+  }
 }
